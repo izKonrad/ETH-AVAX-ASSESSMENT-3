7c290cdb9f0f3124ee7520447a34eb8f04910e27
izKonrad <114236177+izKonrad@users.noreply.github.com>
2024-07-09 19:27:08 +0800

    Add files via upload

---
+++ MyToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor(string memory name, string memory symbol, address initialOwner) ERC20(name, symbol) Ownable(initialOwner) {}
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function transferTokens(address recipient, uint256 amount) public {
+        _transfer(msg.sender, recipient, amount);
+    }
+}
